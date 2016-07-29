a3512b08a1b10b73cd9d0b56995dec19deffc26e
Yara TP <ytercero36@gmail.com>
2016-07-28 20:07:53 -0400

    completed linkedList!

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,16 +2,43 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  var lastNode;
 
   list.addToTail = function(value) {
+
+    var currentNode = Node(value);
+
+    if(list.head === null){
+      list.head = currentNode;
+      list.tail = currentNode;
+    } else{
+      lastNode = list.tail;
+      list.tail = currentNode;
+      lastNode.next = list.tail;
+    }
   };
 
   list.removeHead = function() {
+    var storage = list.head.next;
+    var formerHead = list.head.value;
+
+    delete list.head;
+    list.head = storage;
+    return formerHead;
   };
 
   list.contains = function(target) {
+    var searchNode = function(target, node){
+      if(node.value === target){
+        return true;
+      } else if(node.next === null){
+        return false;
+      } else {
+        return searchNode(target, node.next);
+      };
+    };
+    return searchNode(target, list.head);
   };
-
   return list;
 };
 
@@ -27,3 +54,5 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
