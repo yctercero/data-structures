45b415a97f5ba4527a814a31d9566df12926c104
Yara TP <ytercero36@gmail.com>
2016-07-30 15:10:10 -0400

    added tests to make sure .parent value and .removeFromParent are functioning correctly

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -2,7 +2,7 @@ describe('tree', function() {
   var tree;
 
   beforeEach(function() {
-    tree = Tree();
+    tree = Tree(0);
   });
 
   it('should have methods named "addChild" and "contains", and a property named "value"', function() {
@@ -39,6 +39,19 @@ describe('tree', function() {
     tree.children[1].addChild(8);
     expect(tree.contains(7)).to.equal(true);
     expect(tree.contains(8)).to.equal(true);
+  });
+
+  it('should be able to add value of parent to child nodes', function() {
+    tree.addChild(5);
+    tree.children[0].addChild(6);
+    expect(tree.children[0].children[0].parent.value).to.equal(5);
+  });
+
+  it('should be able to remove parent from child node', function() {
+    tree.addChild(5);
+    tree.children[0].addChild(6);
+    tree.children[0].children[0].removeFromParent();
+    expect(tree.children[0].children).to.deep.equal([]);
   });
 
 });
